aaab89ed18991f5ac75ca738e639a10591a5900d
wevader <wevader1986@gmail.com>
2024-04-29 19:27:14 -0600

    actualzacion metadatos

--- src/components/_Projects.jsx
+++ src/components/_Projects.jsx
@@ -5,16 +5,16 @@ const Projects = () => {
     <section id="projects">
       <h2>My Projects</h2>
       <div>
-        <video controls autoPlay poster="/ecommers.png" src="/Ecommers.mp4"></video>
+        <video controls poster="/ecommers.png" src="/Ecommers.mp4"></video>
       </div>
       <div>
-        <video controls autoPlay poster="/movies.png" src="/streaming.mp4"></video>
+        <video controls poster="/movies.png" src="/streaming.mp4"></video>
       </div>
       <div>
-        <video controls autoPlay poster="/gastos.png" src="/expense-app.mp4"></video>
+        <video controls poster="/gastos.png" src="/expense-app.mp4"></video>
       </div>
       <div>
-        <video controls autoPlay poster="/blog.png" src="/gallery.mp4"></video>
+        <video controls poster="/blog.png" src="/gallery.mp4"></video>
       </div>
 
     </section>
